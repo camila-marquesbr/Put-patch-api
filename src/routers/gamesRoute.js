2471d488251f5c4c0c3e0f5be6b1f2a47b838ec5
camila-marquesbr <camiladev.br@gmail.com>
2020-10-16 00:20:16 -0300

    router update

--- src/routers/gamesRoute.js
+++ src/routers/gamesRoute.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/gamesController')
 const cors = require('cors')
 
 router.put("/games/:id", cors(), controller.atualizar)
+router.patch("/games/:id", cors(), controller.update)
 
 module.exports= router
