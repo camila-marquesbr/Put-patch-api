42f03239362420521971b8faa5db062a8e54de5d
camila-marquesbr <camiladev.br@gmail.com>
2020-10-16 00:12:47 -0300

    estrutura controller

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,12 +1,22 @@
 const games = require('../models/games.json')
 
-const atualizar = (request, response) =>{
+const atualizar = (request, response)=>{
     const gameAtualizado = request.body
-    const id = request.params.id
-    const gameIds = games.map(game=>game.id)
+    const id = parseInt(request.params.id)
 
-    console.log(gameIds)
+    const gameIds = games.map(game => game.id)
+    
+    const atualizaId = gameIds.indexOf(id)
+
+    const gameAtualizadocomId = {id, ...gameAtualizado}
+    musicas.splice(atualizaId, 1, gameAtualizadocomId)
+
+    response.status(200).send(games.find(game => game.id === id))
+    console.log(games)
+     
+
 }
+
 
 module.exports = {
     atualizar
